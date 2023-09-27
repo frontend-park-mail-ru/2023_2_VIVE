52c3134a939fafeddd51bffc6657ddfe03823b71
Vladimir Konoplyuk <konoplyuk.v@yandex.ru>
2023-09-27 16:37:40 +0300

    VIVE-19: Убрать лишний код

--- server/index.js
+++ server/index.js
@@ -1,8 +1,6 @@
 'use strict';
 
 const express = require('express');
-const http = require('http');
-const fs = require('fs');
 const debug = require('debug');
 const path = require('path');
 const app = express();
@@ -14,37 +12,6 @@ app.use(express.static(path.join(__dirname, '..', 'public', 'js')));
 
 const SERVER_PORT = 3000;
 
-// app.use()
-
-app.get('/', (req, res) => {
-  // res.write
-  //   res.send('Hello World!');
-  res.sendFile('index.html')
-});
-
 app.listen(SERVER_PORT, () => {
   debug.log(`Frontend-server listening on port ${SERVER_PORT}`);
 });
-
-// const server = http.createServer((request, response) => {
-//     const {url} = request;
-//     debug.log('url: ' + url);
-//     const normalizedUrl = url === '/' ? '/index.html' : url;
-//     const filepath = `../public${normalizedUrl}`;
-//     debug.log('filepath: ' + filepath);
-
-//     fs.readFile(filepath, (err, data) => {
-//         if (err) {
-//             debug.log('error: ' + JSON.stringify(err));
-//             // response.write(page404);
-//             response.end('NO');
-//             return;
-//         }
-
-//         response.write(data);
-//         response.end();
-//     })
-// });
-
-// debug.log('Starting server...');
-// server.listen(SERVER_PORT);
