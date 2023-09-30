b549bc9ffa3371fd9b3ac3e97130bc387cb41ca9
Vladimir Konoplyuk <konoplyuk.v@yandex.ru>
2023-09-30 19:05:22 +0300

    VIVE-19: Убрать пакет debug

--- server/index.js
+++ server/index.js
@@ -1,17 +1,15 @@
 'use strict';
 
 const express = require('express');
-const debug = require('debug');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, '..', 'public')));
+// app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(express.static(path.join(__dirname, '..', 'public', 'css')));
 app.use(express.static(path.join(__dirname, '..', 'public', 'img')));
 app.use(express.static(path.join(__dirname, '..', 'public', 'js')));
-
 const SERVER_PORT = 3000;
 
 app.listen(SERVER_PORT, () => {
-  debug.log(`Frontend-server listening on port ${SERVER_PORT}`);
+  console.debug(`Frontend-server listening on port ${SERVER_PORT}`)
 });
