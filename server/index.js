0eb8c01686686fedca4310ba05ee6134cc96917f
Vladimir Konoplyuk <konoplyuk.v@yandex.ru>
2023-09-30 19:14:20 +0300

    VIVE-19: Убрать лишние пути для статических файлов

--- server/index.js
+++ server/index.js
@@ -4,10 +4,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// app.use(express.static(path.join(__dirname, '..', 'public')));
-app.use(express.static(path.join(__dirname, '..', 'public', 'css')));
-app.use(express.static(path.join(__dirname, '..', 'public', 'img')));
-app.use(express.static(path.join(__dirname, '..', 'public', 'js')));
+app.use(express.static(path.join(__dirname, '..', 'public')));
 const SERVER_PORT = 3000;
 
 app.listen(SERVER_PORT, () => {
