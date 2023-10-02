715f4c47cf8c174b5939f7f29e27c24e7e9d240b
Vladimir Konoplyuk <konoplyuk.v@yandex.ru>
2023-10-02 21:39:50 +0300

    VIVE-22: Добавить JSDoc к модулю и его методам

--- public/modules/APIConnector.js
+++ public/modules/APIConnector.js
@@ -18,38 +18,81 @@ const REQUEST_HEADERS = {
  * @module APIConnector
  */
 export default {
+  /**
+   * Makes HTTP GET-request to url.
+   * Returns Response if HTTP status is 200-299.
+   *
+   * @param {string} url A string to set request's url.
+   * @throws Will throw an error if HTTP status is not 200-299.
+   * @returns {Response} A response from HTTP GET-request.
+   */
   async get(url) {
-    return await this._sendRequest(url);
+    return this._sendRequest(url);
   },
 
+  /**
+   * Makes HTTP POST-request to url.
+   * Returns Response if HTTP status is 200-299.
+   * 
+   * @param {string} url A string to set request's url.
+   * @param {object} data An object to set request's body.
+   * @throws Will throw an error if HTTP status is not 200-299.
+   * @returns {Response} A response from HTTP POST-request.
+   */
   async post(url, data) {
-    return await this._sendRequest(
-      url,
-      REQUEST_METHODS.POST,
-      JSON.stringify(data)
-    );
+    return this._sendRequest(url, REQUEST_METHODS.POST, JSON.stringify(data));
   },
 
+  /**
+   * Makes HTTP PUT-request to url.
+   * Returns Response if HTTP status is 200-299.
+   * 
+   * @param {string} url A string to set request's url.
+   * @param {object} data An object to set request's body.
+   * @throws Will throw an error if HTTP status is not 200-299.
+   * @returns {Response} A response from HTTP PUT-request.
+   */
   async put(url, data) {
-    return await this._sendRequest(
-      url,
-      REQUEST_METHODS.PUT,
-      JSON.stringify(data)
-    );
+    return this._sendRequest(url, REQUEST_METHODS.PUT, JSON.stringify(data));
   },
 
+  /**
+   * Makes HTTP PATCH-request to url.
+   * Returns Response if HTTP status is 200-299.
+   * 
+   * @param {string} url A string to set request's url.
+   * @param {object} data An object to set request's body.
+   * @throws Will throw an error if HTTP status is not 200-299.
+   * @returns {Response} A response from HTTP PUT-request.
+   */
   async patch(url, data) {
-    return await this._sendRequest(
-      url,
-      REQUEST_METHODS.PATCH,
-      JSON.stringify(data)
-    );
+    return this._sendRequest(url, REQUEST_METHODS.PATCH, JSON.stringify(data));
   },
 
+
+  /**
+   * Makes HTTP DELETE-request to url.
+   * Returns Response if HTTP status is 200-299.
+   * 
+   * @param {string} url A string to set request's url.
+   * @throws Will throw an error if HTTP status is not 200-299.
+   * @returns {Response} A response from HTTP PUT-request.
+   */
   async delete(url) {
-    return await this._sendRequest(url, REQUEST_METHODS.DELETE);
+    return this._sendRequest(url, REQUEST_METHODS.DELETE);
   },
 
+  /**
+   * Makes HTTP request to url.
+   * Returns Response if HTTP status is 200-299.
+   * Throws an error otherwise.
+   *
+   * @param {string} url A string to set request's url.
+   * @param {REQUEST_METHODS} [method=REQUEST_METHODS.GET] A string to set request's method.
+   * @param {string} [body=null] A stringfied json to set request's body.
+   * @throws Will throw an error if HTTP status is not 200-299.
+   * @returns {Response} A response from HTTP request.
+   */
   async _sendRequest(url, method = REQUEST_METHODS.GET, body = null) {
     let response = await fetch(url, {
       method: method,
