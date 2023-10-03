197bbf55b2278ba078e621bf267a0ed3c260a90e
Davydov Egor <58881280+Butters7@users.noreply.github.com>
2023-10-03 17:38:39 +0300

    VIVE-26 + VIVE-23

--- server/index.js
+++ server/index.js
@@ -11,33 +11,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'templates', 'index.html'));
 });
 
-app.get('/templates/employer_reg.html', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'templates', 'employer_reg.html'));
-});
-
-app.get('/templates/employer_sign_in.html', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'templates', 'employer_sign_in.html'));
-});
-
-app.get('/templates/applicant_reg.html', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'templates', 'applicant_reg.html'));
-});
-
-app.get('/templates/applicant_sign_in.html', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'templates', 'applicant_sign_in.html'));
-});
-
-app.get('/session', (req, res) => {
-  const isAuth = Math.random() > 0.5;
-
-  if (!isAuth) {
-    res.cookie('cooka', 435643, {expires: new Date(Date.now() + 1000 * 60 * 10)});
-    res.status(200).json({body : 435643});
-  }
-
-  res.json({isAuth : isAuth});
-});
-
 app.listen(SERVER_PORT, () => {
   console.debug(`Frontend-server listening on port ${SERVER_PORT}`)
 });
