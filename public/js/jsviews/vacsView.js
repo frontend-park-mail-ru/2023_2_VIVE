d8407aee103bdf8402537c012de7e0abb6c69579
Ilya Aleshin <idaleshin@mail.ru>
2023-10-04 15:55:52 +0300

    test 1

--- public/js/jsviews/vacsView.js
+++ public/js/jsviews/vacsView.js
@@ -1,11 +1,21 @@
+import { BACKEND_SERVER_URL } from "../../../config/config.js";
+import APIConnector from "../../modules/APIConnector.js";
 import { router } from "../router/router.js";
 
 export default class vacsView {
   render() {
     console.log("rendering vacs");
     const template = Handlebars.templates["vacs.hbs"];
-    document.querySelector("main").innerHTML = template();
+    let data = this.getVacancies();
+    document.querySelector("main").innerHTML = template(data);
     this.addEventListeners();
+  }
+
+  getVacancies() {
+    return APIConnector.get(BACKEND_SERVER_URL + '/vacancies')
+    .then(resp => resp.json())
+    // .then(json => json)
+    .catch(error => console.error(error));
   }
 
   addEventListeners() {
