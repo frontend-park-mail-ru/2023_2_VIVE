0347bd18453a6da5df00ec338304a58e49b99862
Butters7 <egorka.davydov.1980@mail.ru>
2023-10-04 12:34:35 +0300

    Revert "change port"
    This reverts commit d8a95a062d792bb5a067985b8eb94470aefa10fd.

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.engine(
 app.set("view engine", "hbs");
 
 app.use(express.static(path.join(__dirname, "..")));
-const SERVER_PORT = 8082;
+const SERVER_PORT = 3000;
 
 app.get("/", (req, res) => {
   res.render("pages/vacancies");
