772ea91a9c62317ca20c85355f7958e61a0e381d
Butters7 <egorka.davydov.1980@mail.ru>
2023-10-04 18:19:47 +0300

    add data length

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,4 @@
 import { cookie } from "./cookieCheck/cookieCheck.js";
 
 let cur_path = window.location.pathname;
-await cookie.checkPathForNoCookie(cur_path);
+cookie.checkPathForNoCookie(cur_path);
