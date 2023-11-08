3888d7b5212534bb3d4e5ca2cabffc8b54ef25ab
Ilya Aleshin <idaleshin@mail.ru>
2023-11-08 11:44:37 +0300

    fix rescr_form

--- public/js/views/resCreationView.js
+++ public/js/views/resCreationView.js
@@ -15,9 +15,9 @@ export default class resCreationView extends View {
     super.render();
     // eslint-disable-next-line no-undef
     const template = Handlebars.templates['res_creation'];
-    document.querySelector('main').innerHTML = template({ 
-      user: await User.getUser(), 
-      page: this.page 
+    document.querySelector('main').innerHTML = template({
+      user: await User.getUser(),
+      page: this.page
     });
 
     this.addEventListeners();
@@ -27,15 +27,18 @@ export default class resCreationView extends View {
   addEventListeners() {
     const form = document.querySelector('.rescr__form')
     const save_cont_btn = document.querySelector('.js-rescr-save-continue');
-    save_cont_btn.addEventListener('click', event => {
-      event.preventDefault();
-      this.page++;
-      this.render();
-      const data = getFormObject(new FormData(form));
-      // TODO валидация формы
-      console.log(data);
-      
-    })
+    if (save_cont_btn) {
+      save_cont_btn.addEventListener('click', event => {
+        event.preventDefault();
+        this.page++;
+        this.render();
+        const cur_data = getFormObject(new FormData(form));
+        // TODO валидация формы
+        console.log(cur_data);
+        Object.assign(this.data, cur_data);
+        console.log(this.data);
+      })
+    }
 
     const back_btn = document.querySelector('.js-rescr-back');
     if (back_btn) {
@@ -45,10 +48,50 @@ export default class resCreationView extends View {
         this.render();
       })
     }
+
+    const submit_btn = document.querySelector(".js-rescr-submit");
+    if (submit_btn) {
+      submit_btn.addEventListener('click', event => {
+        event.preventDefault();
+        const cur_data = getFormObject(new FormData(form));
+        // TODO валидация формы
+        console.log(cur_data);
+      })
+    }
+  }
+
+  addListenersToExpForm() {
+    const is_end_date = document.querySelector(".js-name-is-expirience_end_date");
+    const block_end_date = document.querySelector(".js-block-expirience_end_date");
+    if (!is_end_date.checked) {
+      block_end_date.innerHTML = Handlebars.partials['res_form_exp_end_date']();
+    }
+    is_end_date.addEventListener('change', event => {
+      if (is_end_date.checked) {
+        block_end_date.innerHTML = "";
+      } else {
+        block_end_date.innerHTML = Handlebars.partials['res_form_exp_end_date']();
+      }
+    })
   }
 
   addEventListenersToPage() {
-    
+    if (this.page == 3) {
+      const is_exp = document.querySelector(".js-name-is-experience");
+      const exp_form = document.querySelector(".js-res-form-exp");
+      is_exp.addEventListener('change', event => {
+        if (is_exp.checked) {
+          exp_form.innerHTML = Handlebars.partials['res_form_exp']();
+          if (!this.addedListenersToExpForm) {
+            this.addedListenersToExpForm = true;
+            this.addListenersToExpForm();
+          }
+        } else {
+          exp_form.innerHTML = "";
+        }
+      })
+
+    }
   }
 
 }
