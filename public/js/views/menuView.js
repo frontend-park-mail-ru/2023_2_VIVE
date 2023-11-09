f2a1837caec6b543fcfb2c7ead6251eef37085c7
Butters7 <egorka.davydov.1980@mail.ru>
2023-11-10 00:31:29 +0300

    finish drop content menu

--- public/js/views/menuView.js
+++ public/js/views/menuView.js
@@ -1,3 +1,5 @@
+import { BACKEND_SERVER_URL } from '../../../config/config.js';
+import APIConnector from '../modules/APIConnector.js';
 import router from '../modules/router.js';
 import User from '../stores/userStore.js';
 import { getHrefFromLink } from '../utils.js';
@@ -36,9 +38,61 @@ export default class menuView extends View {
       });
     });
 
+    const dropdownBtn = document.querySelector('.dropdown__padd-btn');
+    const content = document.querySelector('.dropdown__content');
+
+    if (dropdownBtn) {
+      dropdownBtn.addEventListener('click', function (event) {
+        const isContentVisible = !content.classList.contains('d-none');
+        
+        if (isContentVisible) {
+          dropdownBtn.classList.remove('dropdown__img--rotate');
+          dropdownBtn.classList.add('dropdown__img--rotate-secondary');
+          content.classList.add('d-none');
+        } else {
+          dropdownBtn.classList.remove('dropdown__img--rotate-secondary');
+          dropdownBtn.classList.add('dropdown__img--rotate');
+          content.classList.remove('d-none');
+        }
+
+        event.stopPropagation();
+      });
+
+      document.addEventListener('click', function (event) {
+        if (!content.contains(event.target) && !dropdownBtn.contains(event.target)) {
+          dropdownBtn.classList.remove('dropdown__img--rotate');
+          dropdownBtn.classList.add('dropdown__img--rotate-secondary');
+          content.classList.add('d-none');
+        }
+      });
+
+      const profileBtn = document.querySelector('[name="profile"]');
+      const settingBtn = document.querySelector('[name="settings"]');
+      const switchBtn = document.querySelector('[name="switch"]');
+      const leaveBtn = document.querySelector('[name="leave"]');
+  
+      profileBtn.addEventListener('click', () => {
+          router.goToLink('/profile');
+      });
+  
+      settingBtn.addEventListener('click', () => {
+        router.goToLink('/profile/settings');
+      });
+  
+      leaveBtn.addEventListener('click', async (e) => {
+        e.preventDefault();
+        try {
+          await APIConnector.delete(BACKEND_SERVER_URL + '/session');
+          router.goToLink('/');
+        } catch (err) {
+          console.error('logout: ', err);
+        }
+      });
+    }
+
     // if (await cookie.hasCookie()) {
     //   const logout_btn = document.querySelector('.logout-btn');
-    //   logout_btn.addEventListener('click', async (e) => {
+    //   leaveBtn.addEventListener('click', async (e) => {
     //     e.preventDefault();
     //     try {
     //       await APIConnector.delete(BACKEND_SERVER_URL + '/session');
