63f4c9e87eaca6deaccfe939958ef5c6ce20a5f8
Butters7 <egorka.davydov.1980@mail.com>
2023-11-09 15:27:40 +0000

    profile adn vacancy to bem

--- public/js/views/profileView.js
+++ public/js/views/profileView.js
@@ -19,6 +19,7 @@ export default class profileView extends View {
     let data = {};
 
     if (this.state == 'vacancies') {
+      console.log(200);
       data = await this.getUserVacancies();
     }
 
