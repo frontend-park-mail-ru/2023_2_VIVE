ddb43fd946801c75a5122cdf0b69fd0ac0589b11
Butters7 <egorka.davydov.1980@mail.ru>
2023-11-24 20:22:17 +0300

    Вспомогательные кнопки внутри выпадающего меню

--- public/js/views/mainView.js
+++ public/js/views/mainView.js
@@ -19,45 +19,53 @@ export default class mainView extends View {
     addEventListeners() {
         super.addEventListeners();
 
-        const dropdownBtn = document.querySelector('.dropdown__padd-btn');
-        const content = document.querySelector('.dropdown__content');
+        const profileDropdown = document.querySelector('[name="drop-btn-profile"]');
+        const profileContentDropdown = document.querySelector('.dropdown__content__profile');
 
-        if (dropdownBtn) {
-            dropdownBtn.addEventListener('click', function (event) {
-                const isContentVisible = !content.classList.contains('d-none');
+        const addInfoDropdown = document.querySelector('[name="drop-btn-add"');
+        const addInfoContentDropdown = document.querySelector('.dropdown__content-add-info');
+        const addInfoNav = document.querySelector('.navbar__item__drp-menu');
+
+        if (profileDropdown) {
+            profileDropdown.addEventListener('click', function (event) {
+                const isContentVisible = !profileContentDropdown.classList.contains('d-none');
 
                 if (isContentVisible) {
-                    dropdownBtn.classList.remove('dropdown__img--rotate');
-                    dropdownBtn.classList.add('dropdown__img--rotate-secondary');
-                    content.classList.add('d-none');
+                    profileDropdown.classList.remove('dropdown__img--rotate');
+                    profileDropdown.classList.add('dropdown__img--rotate-secondary');
+                    profileContentDropdown.classList.add('d-none');
                 } else {
-                    dropdownBtn.classList.remove('dropdown__img--rotate-secondary');
-                    dropdownBtn.classList.add('dropdown__img--rotate');
-                    content.classList.remove('d-none');
+                    profileDropdown.classList.remove('dropdown__img--rotate-secondary');
+                    profileDropdown.classList.add('dropdown__img--rotate');
+                    profileContentDropdown.classList.remove('d-none');
                 }
 
                 event.stopPropagation();
             });
 
             document.addEventListener('click', function (event) {
-                if (!content.contains(event.target) && !dropdownBtn.contains(event.target)) {
-                    dropdownBtn.classList.remove('dropdown__img--rotate');
-                    dropdownBtn.classList.add('dropdown__img--rotate-secondary');
-                    content.classList.add('d-none');
+                if (!profileContentDropdown.contains(event.target) && !profileDropdown.contains(event.target)) {
+                    profileDropdown.classList.remove('dropdown__img--rotate');
+                    profileDropdown.classList.add('dropdown__img--rotate-secondary');
+                    profileContentDropdown.classList.add('d-none');
                 }
             });
-
-            const profileBtn = document.querySelector('[name="profile"]');
-            const settingBtn = document.querySelector('[name="settings"]');
+            
             const switchBtn = document.querySelector('[name="switch"]');
             const leaveBtn = document.querySelector('[name="leave"]');
 
-            profileBtn.addEventListener('click', () => {
-                router.goToLink('/profile');
-            });
-
-            settingBtn.addEventListener('click', () => {
-                router.goToLink('/profile/settings');
+            switchBtn.addEventListener('click', async (e) => {
+                e.preventDefault();
+                try {
+                    await User.logout();
+                    if (User.getUser().role === User.ROLES.app) {
+                        router.goToLink('/app_auth');
+                    } else {
+                        router.goToLink('/emp_auth');
+                    }
+                } catch (err) {
+                    console.error('logout: ', err);
+                }
             });
 
             leaveBtn.addEventListener('click', async (e) => {
@@ -67,6 +75,29 @@ export default class mainView extends View {
                     router.goToLink('/');
                 } catch (err) {
                     console.error('logout: ', err);
+                }
+            });
+        }
+
+        if (addInfoDropdown) {
+            addInfoDropdown.addEventListener('click', function (event) {
+                const isContentVisible = !addInfoContentDropdown.classList.contains('d-none');
+
+                if (isContentVisible) {
+                    addInfoContentDropdown.classList.add('d-none');
+                    addInfoNav.classList.remove('minus');
+                } else {
+                    addInfoContentDropdown.classList.remove('d-none');
+                    addInfoNav.classList.add('minus');
+                }
+
+                event.stopPropagation();
+            });
+
+            document.addEventListener('click', function (event) {
+                if (!addInfoContentDropdown.contains(event.target) && !addInfoDropdown.contains(event.target)) {
+                    addInfoNav.classList.remove('minus');
+                    addInfoContentDropdown.classList.add('d-none');
                 }
             });
         }
