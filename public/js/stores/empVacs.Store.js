327654223638d30f7db1ee7580e2f1465a06bcd5
Butters7 <egorka.davydov.1980@mail.ru>
2023-12-09 02:29:39 +0300

    empVacs mobile adaptive

--- public/js/stores/empVacs.Store.js
+++ public/js/stores/empVacs.Store.js
@@ -2,6 +2,7 @@ import { BACKEND_SERVER_URL } from "../../../config/config.js";
 import APIConnector from "../modules/APIConnector.js";
 import Store from "./Store.js";
 import User from "./UserStore.js";
+import vacancyStore from "./VacancyStore.js";
 
 class EmpVacsStore extends Store {
     constructor() {
@@ -28,6 +29,11 @@ class EmpVacsStore extends Store {
             this.organization = resp_json.organization_name;
             this.full_name = resp_json.last_name + " " + resp_json.first_name;
             this.vacancies = resp_json.vacancies;
+            this.vacancies.forEach(element => {
+                element.salary_view = vacancyStore.processVacanciesSalary(element);
+                element.employment_view = vacancyStore.processVacanciesEmployment(element);
+                element.expirience_view = vacancyStore.processVacanciesExperience(element);
+            });
             this.user = User.getUser();
             return true;
         } catch(error) {
